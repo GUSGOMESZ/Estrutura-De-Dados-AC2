0cf6e4e8a7b0e37f1af2dcf653ac7f4362c4821f
GustavoGomes <gomesgustavo1504@gmail.com>
2025-05-08 21:55:03 -0300

    feat: list remove tests

---
+++ TestesListaDinamica/Remocao/remocao40k.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "../video.h"
+
+int main() {
+    PointerVideo lista = NULL;
+    clock_t inicio, fim;
+    double tempo;
+
+    criaVideoLista(&lista);
+
+    insercaoVideoLista(&lista, 40000);
+
+    inicio = clock();
+
+    liberaListaVideo(&lista);
+
+    fim = clock();
+
+    tempo = (double)(fim - inicio) / CLOCKS_PER_SEC;
+    printf("Tempo para remover 40.000 elementos: %.4f segundos\n", tempo);
+
+    return 0;
+}
